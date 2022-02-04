2229733c573f8a39e152df483eaa7fb924210518
ppxxxg22 <liujiaxin011121@gmail.com>
2022-02-04 17:08:16 +0800

    shi tomasi

--- c10-corner_detectiion/shi_tomasi/shi_tomasi.cpp
+++ c10-corner_detectiion/shi_tomasi/shi_tomasi.cpp
@@ -41,6 +41,20 @@ void on_good_feature_to_track(int, void *)
     Mat copy = img.clone();
 
     goodFeaturesToTrack(gray, corners, max_corner_num, quality_level, min_distance, Mat(), blocksize, false, k);
+    /*
+     * 确定图像强角点函数，是harris角点检测的改进版本
+     * 第一个参数是输入图像，单通道8位或浮点
+     * 第二个参数是输出的角点向量（vector数组）
+     * 第三个参数是角点的最多数目
+     * 第四个参数是角点检测接受的最小特征值（一般取0.1或0.01）
+     * 第五个参数是角点之间最小距离
+     * 第六个参数是掩模mask，单通道同尺寸
+     * 第七个参数是导数自相关矩阵的邻域范围
+     * 第八个参数是是否使用harris检测
+     * 第九个参数是设置自相关矩阵行列式的权重，有默认值0.04
+     * 
+     * 此函数还可以初始化基于点的对象跟踪操作
+    */
 
     printf("角点检测数目 : %d \n", corners.size());
 
